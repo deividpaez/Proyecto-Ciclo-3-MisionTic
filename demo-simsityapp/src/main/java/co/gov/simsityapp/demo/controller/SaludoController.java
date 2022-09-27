0fdd5c6bed13637562a44fa0a94b27fb233e6706
deividpaez <deivid11pdp1@hotmail.com>
2022-09-27 15:32:29 -0500

    se agregaron funcionalidades en el Front End

--- demo-simsityapp/src/main/java/co/gov/simsityapp/demo/controller/SaludoController.java
+++ demo-simsityapp/src/main/java/co/gov/simsityapp/demo/controller/SaludoController.java
@@ -1,6 +1,8 @@
 package co.gov.simsityapp.demo.controller;
 
 
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/saludo")
+
 public class SaludoController {
  @GetMapping
+ 
  public String saludo(@RequestParam("name") String name){
-  return String.format(" Su dato ha sido procesado Sr/a: %s", name);
+    
+  return String.format(" Su dat<zx<zx<zx<zx<o ha sido procesado Sr/a: %s", name);
  }
 }
--- demo-simsityapp/src/main/resources/static/js/controller_help.js
+++ demo-simsityapp/src/main/resources/static/js/controller_help.js
@@ -1,10 +1,41 @@
-let btn_Help = document.getElementById('appointment');
-
-btn_Help.addEventListener("click", (e)=>{
-  let namee = prompt("Ingrese su nombre");
-  fetch(`http://localhost:8090/api/saludo?name=${namee}`, { method: 'GET' })
-  .then(response => response.text())
-  .then(data =>{
-    console.log(data)
-  })
+let btn_Help = document.getElementById('appointment'),
+btn_Transito = document.getElementById("Transito"),
+btn_Bomberos = document.getElementById("Bomberos"),
+btn_Medico = document.getElementById("Medico"),
+Btn_Ubicacion = document.getElementById("Ubication")
+let nombre;
+async function recibir_nombre (name){
+nombre = name;
+await fetch(`http://localhost:8090/api/saludo?name=${name}`, { method: 'GET' })
+ .then(response => response.text())
+.then(data =>{
+  alert( data)
 })
+}
+btn_Help.addEventListener("click", (e)=>{
+  nombre = prompt("Ingrese su nombre")
+recibir_nombre(nombre);
+})
+ btn_Transito.addEventListener("click", (e)=>{
+ 
+ })
+ btn_Bomberos.addEventListener("click", (e)=>{
+  alert("LLAMANDO A BOMBEROS")
+ })
+ btn_Medico.addEventListener("click", (e)=>{
+  alert("LLAMANDO UNA AMBULANCIA")
+ })
+
+ Ubication.addEventListener("click", (e)=>{
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarUbicacion);
+  }
+  
+  function mostrarUbicacion (ubicacion) {
+    const lng = ubicacion.coords.longitude;
+    const lat = ubicacion.coords.latitude;
+    console.log(`longitud: ${ lng } | latitud: ${ lat }`);
+  }
+ })
+
+
